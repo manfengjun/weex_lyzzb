d8bca1a6c9bda64093bf4d5ae99f7fc8d6708e3d
jun <2546863989@qq.com>
2017-12-04 18:01:27 +0800

    JUN 完成Tabbar

--- Section/Home/HomeViewController.swift
+++ Section/Home/HomeViewController.swift
@@ -1,31 +1,38 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  LYZZB_weex
 //
-//  Created by jun on 2017/11/28.
+//  Created by jun on 2017/12/4.
 //  Copyright © 2017年 JUN. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: BaseViewController {
     var webViewModel: FJWebViewModel?
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let path = "file://" + Bundle.main.bundlePath + "/lyzzb/js/home/home.js"
         let viewF = CGRect(x: 0, y: 64, width: SCREEN_WIDTH, height: SCREEN_HEIGHT - 64)
         webViewModel = FJWebViewModel(path: path, target: self, frame:viewF)
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view.
     }
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
-}
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
